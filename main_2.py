35d90a5b142cf0c309def268d25d701c2830325e
Julian Mierzejewski <j3mierz@gmail.com>
2024-04-14 16:25:18 +0200

    recersed guessing game

---
+++ main_2.py
@@ -0,0 +1,7 @@
+# reversed Guessing game
+
+print('think of number between 1 and 1000 i will guess it in max 10 tries')
+
+def computer_next_number(min, max):
+    return  int((max - min) / 2 + min)
+
