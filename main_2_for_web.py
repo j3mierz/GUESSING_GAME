b0cc92b91a1df85ead8544fd26e4b6af2debc6ba
Julian Mierzejewski <j3mierz@gmail.com>
2024-04-14 19:47:50 +0200

    working game

--- main_2_for_web.py
+++ main_2_for_web.py
@@ -1,11 +1,30 @@
 # reversed guessing game for web app
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def reversed_guessing_game():
+def computer_next_number(a, b):
+    return int((b - a) / 2 + a)
 
-    return render_template('guessing_game_form.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def reversed_guessing_game():
+    if request.method == 'GET':
+        return render_template('guessing_game_form.html', min=1, max=1000, guess=500)
+    if request.method == "POST":
+        max = int(request.form.get('max'))
+        min = int(request.form.get('min'))
+        guess = int(request.form.get("guess", 500))
+        user_answer = request.form.get('user_answer')
+
+        if user_answer == 'too small':
+            min = guess
+        if user_answer == 'too big':
+            max = guess
+        elif user_answer == 'you won':
+            return render_template('guessing_game_form.html', guess=guess, min=min, max=max, message='I won')
+        guess = computer_next_number(max, min)
+
+        return render_template('guessing_game_form.html', guess=guess, min=min, max=max)
