3b3810376c1495c240e422a5e97a01deb4bd76a3
Julian Mierzejewski <j3mierz@gmail.com>
2024-04-14 16:44:35 +0200

    playing loop function added

--- main_2.py
+++ main_2.py
@@ -2,6 +2,29 @@
 
 print('think of number between 1 and 1000 i will guess it in max 10 tries')
 
-def computer_next_number(min, max):
-    return  int((max - min) / 2 + min)
 
+# next number equation
+def computer_next_number(a, b):
+    return int((b - a) / 2 + a)
+
+
+# playing loop
+def player_guessing():
+    max_2 = 1000
+    min_2 = 1
+    print(computer_next_number(min_2, max_2))
+    answer = input()
+    while not answer == "you win":
+
+        if answer == 'too small':
+            min_2 = computer_next_number(min_2, max_2)
+        if answer == 'too big':
+            max_2 = computer_next_number(min_2, max_2)
+
+        print(computer_next_number(min_2, max_2))
+        answer = input()
+    print('I won')
+    return
+
+
+player_guessing()
